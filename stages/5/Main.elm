735138d81da837f9a29c558079d51af06d94e8d3
sdtsui <szehungdanieltsui@gmail.com>
2016-03-26 16:40:06 -0700

    stage5 complete

--- stages/5/Main.elm
+++ stages/5/Main.elm
@@ -23,9 +23,12 @@ app =
     { view = view
     , update = update
     , init = ( initialModel, Effects.task (searchFeed initialModel.query) )
+    -- example of a search: fully featured/ used in production
     , inputs = []
     }
-
+-- hints:
+    -- get decoder working: everying shows up on page
+    -- THEN, type in ::: page should update
 
 port tasks : Signal (Task Effects.Never ())
 port tasks =
@@ -58,9 +61,9 @@ searchResultDecoder =
   Json.Decode.object3
     SearchResult
     -- See https://developer.github.com/v3/search/#example
-    ("TODO what field goes here?" := Json.Decode.int)
-    ("TODO what field goes here?" := Json.Decode.string)
-    ("TODO what field goes here?" := Json.Decode.int)
+    ("id" := Json.Decode.int)
+    ("name" := Json.Decode.string)
+    ("stargazers_count" := Json.Decode.int)
 
 
 type alias Model =
@@ -97,7 +100,7 @@ view address model =
         , span [ class "tagline" ] [ text "“Like GitHub, but for Elm things.”" ]
         ]
     , input [ class "search-query", onInput address SetQuery, defaultValue model.query ] []
-    , button [ class "search-button" {- TODO on click, run a search -} ] [ text "Search" ]
+    , button [ class "search-button", onClick address Search ] [ text "Search" ]
     , ul
         [ class "results" ]
         (List.map (viewSearchResult address) model.results)
@@ -137,7 +140,7 @@ update : Action -> Model -> ( Model, Effects Action )
 update action model =
   case action of
     Search ->
-      ( model, Effects.none {- TODO use searchFeed to run a search -} )
+      ( model, Effects.task (searchFeed model.query) )
 
     SetQuery query ->
       ( { model | query = query }, Effects.none )
