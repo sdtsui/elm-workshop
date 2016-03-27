3aa4abd14481f12052287f00ab6dc7359aff0548
sdtsui <szehungdanieltsui@gmail.com>
2016-03-27 10:11:20 -0700

    notes update

---
+++ stages/overview-cliffs.elm
@@ -0,0 +1,12 @@
+1 - basics (infix style), boilerplate, Html
+2 - syntax, operators
+3 - actions, type aliases, types, main, startapp
+4 - events, update
+5 - decoder, effects (Effects.none, Effects.all), Tasks (success, fail)
+6 - testing
+7
+8
+9
+10
+misc, lookups: 
+  - converting react components / css into -> Elm views.
